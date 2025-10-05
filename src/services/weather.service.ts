d1d21463f6345c52c4e984974bba57d8d92a3575
Igor Wilian Faoro <igor.faoro17@gmail.com>
2025-10-05 17:17:57 -0300

    asdfa

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,3 +1,5 @@
+import { weatherApi } from '@/core/apis/weather.api';
+
 export interface WeatherDto {
   /** YYYY-MM-DD */
   date: string;
@@ -19,23 +21,44 @@ export interface WeatherResult {
 }
 
 export const createWeatherService = () => {
-  const processWeatherData = (dto: WeatherDto): Promise<WeatherResult> => {
-    return Promise.resolve({
-      temperature: 23,
-      windSpeed: 30,
-      precipitationPercentage: 0,
-      description:
-        'Um dia agradável e ensolarado para o casamento externo, com uma brisa moderada trazendo frescor.',
-      insights: [
-        'Certifique-se de fixar bem as decorações e arranjos devido ao vento.',
-        'Forneça sombra para os convidados, especialmente idosos e crianças.',
-        'Aproveite o clima para atividades ao ar livre e fotos memoráveis.'
-      ]
-    });
+  const processWeatherData = async (
+    dto: WeatherDto
+  ): Promise<WeatherResult> => {
+    // return Promise.resolve({
+    //   temperature: 23,
+    //   windSpeed: 30,
+    //   precipitationPercentage: 0,
+    //   description:
+    //     'Um dia agradável e ensolarado para o casamento externo, com uma brisa moderada trazendo frescor.',
+    //   insights: [
+    //     'Certifique-se de fixar bem as decorações e arranjos devido ao vento.',
+    //     'Forneça sombra para os convidados, especialmente idosos e crianças.',
+    //     'Aproveite o clima para atividades ao ar livre e fotos memoráveis.'
+    //   ]
+    // });
 
-    // return weatherApi
-    //   .post('/api/weather', dto)
-    //   .then((response) => response.data);
+    try {
+      const response = await weatherApi.post('/api/weather', dto, {
+        timeout: 15000
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      
+      return {
+        temperature: 23,
+        windSpeed: 30,
+        precipitationPercentage: 0,
+        description:
+          'Um dia agradável e ensolarado para o casamento externo, com uma brisa moderada trazendo frescor.',
+        insights: [
+          'Certifique-se de fixar bem as decorações e arranjos devido ao vento.',
+          'Forneça sombra para os convidados, especialmente idosos e crianças.',
+          'Aproveite o clima para atividades ao ar livre e fotos memoráveis.'
+        ]
+      };
+    }
   };
 
   return {
