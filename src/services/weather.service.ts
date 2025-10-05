4ac508427b84df5ca77f16a17086e03e8ecc8428
Igor Wilian Faoro <igor.faoro17@gmail.com>
2025-10-05 17:13:43 -0300

    sdafdas

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,5 +1,3 @@
-import { weatherApi } from '@/core/apis/weather.api';
-
 export interface WeatherDto {
   /** YYYY-MM-DD */
   date: string;
@@ -22,9 +20,22 @@ export interface WeatherResult {
 
 export const createWeatherService = () => {
   const processWeatherData = (dto: WeatherDto): Promise<WeatherResult> => {
-    return weatherApi
-      .post('/api/weather', dto)
-      .then((response) => response.data);
+    return Promise.resolve({
+      temperature: 23,
+      windSpeed: 30,
+      precipitationPercentage: 0,
+      description:
+        'Um dia agradável e ensolarado para o casamento externo, com uma brisa moderada trazendo frescor.',
+      insights: [
+        'Certifique-se de fixar bem as decorações e arranjos devido ao vento.',
+        'Forneça sombra para os convidados, especialmente idosos e crianças.',
+        'Aproveite o clima para atividades ao ar livre e fotos memoráveis.'
+      ]
+    });
+
+    // return weatherApi
+    //   .post('/api/weather', dto)
+    //   .then((response) => response.data);
   };
 
   return {
